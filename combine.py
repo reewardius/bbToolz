ed8d4674cf5f7f74214b4e9d47997a4dad30b396
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-08-11 19:45:21 +0300

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,24 +1,44 @@
 import sys
 
-def add_suffix_to_lines(input_file_path, suffix_file_path, output_file_path):
-    with open(input_file_path, 'r') as input_file, \
-         open(suffix_file_path, 'r') as suffix_file, \
-         open(output_file_path, 'w') as output_file:
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    return lines
 
-        suffix = suffix_file.readline().strip()
+def write_file(file_path, content):
+    with open(file_path, 'w') as f:
+        f.writelines(content)
 
-        for line in input_file:
-            line = line.strip()
-            new_line = f"{line}{suffix}\n"
-            output_file.write(new_line)
+def add_suffixes(targets, suffixes):
+    result = []
+    for target in targets:
+        target = target.strip()  # Удаляем символы новой строки и пробелы с обеих сторон
+        if target:
+            for suffix in suffixes:
+                result.append(target + suffix + '\n')  # Добавляем символ новой строки после суффикса
+    return result
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script_name.py input_file_path suffix_file_path output_file_path")
+        print("Использование: python script.py <файл_с_таргетами> <файл_с_суффиксами> result.txt")
         sys.exit(1)
 
-    input_file_path = sys.argv[1]
-    suffix_file_path = sys.argv[2]
+    targets_file_path = sys.argv[1]
+    suffixes_file_path = sys.argv[2]
     output_file_path = sys.argv[3]
 
-    add_suffix_to_lines(input_file_path, suffix_file_path, output_file_path)
+    targets = read_file(targets_file_path)
+    suffixes = read_file(suffixes_file_path)
+
+    targets = [target.strip() for target in targets if target.strip()]
+
+    if not suffixes:
+        print("Файл с суффиксами пуст.")
+        sys.exit(1)
+
+    modified_targets = add_suffixes(targets, suffixes)
+    
+    # Объединяем строки и убираем пустые строки
+    modified_content = "".join(modified_targets).strip()
+
+    write_file(output_file_path, modified_content)
